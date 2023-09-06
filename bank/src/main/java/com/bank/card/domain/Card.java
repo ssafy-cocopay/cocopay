f930909296ae437d2cad3303bc0a92b4afc946b7
HongJiMin <baker0929@naver.com>
2023-09-06 15:32:24 +0900

    feat : 카드 엔티티 생성

--- bank/src/main/java/com/bank/card/domain/Card.java
+++ bank/src/main/java/com/bank/card/domain/Card.java
@@ -15,5 +15,19 @@ public class Card {
     @JoinColumn(name = "bank_id")
     private Bank bank;
 
+    @Column(name = "card_name")
+    private String cardName;
+
+    @Column(name = "performance")
+    private Integer performance;
     
+    @Column(name = "type")
+    private String type;
+
+    /*
+    type 부분 string으로 해놓을까? Enum으로 해야되지 않을까?
+    DB도 varchar가 아니라 tiny int로 해야 될거같기도 함
+     */
+
+
 }
