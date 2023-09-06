0a93059be6eae0f85f19738ad5b1dc1c4a40ac5f
한성현 <hsh5349@kakao.com>
2023-09-06 13:09:33 +0900

    S09P22B208-22 feat : user entity 생성

---
+++ bank/src/main/java/com/bank/user/entity/User.java
@@ -0,0 +1,38 @@
+package com.bank.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer uuid;
+
+    @Column(length = 30 ,nullable = false)
+    private String email;
+
+    @Column(length = 75, nullable = false)
+    private String password;
+
+    @Column(length = 30, nullable = false)
+    private String name;
+
+    @Column(length = 13, nullable = false)
+    private String tel;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime registedDate = LocalDateTime.now();
+
+    private LocalDateTime withdrawDate;
+}
