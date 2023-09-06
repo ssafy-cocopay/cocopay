7f29504baf5d2fc781f1df1876be3142b26c1c24
한성현 <hsh5349@kakao.com>
2023-09-06 15:22:26 +0900

    S09P22B208-22 feat : 회원 조회 동적쿼리 생성

--- bank/src/main/java/com/bank/user/repository/UserRepositoryCustom.java
+++ bank/src/main/java/com/bank/user/repository/UserRepositoryCustom.java
@@ -1,4 +1,10 @@
 package com.bank.user.repository;
 
+import com.bank.user.entity.User;
+
+import java.util.Optional;
+
 public interface UserRepositoryCustom {
+
+    public Optional<User> findUser(Integer uuid, String tel);
 }
