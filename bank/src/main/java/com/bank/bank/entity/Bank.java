1bf74490b397bfe066bf3ea5b76a44fc316a3e0e
hwanhee <dup07168@naver.com>
2023-09-07 16:51:15 +0900

    refactor : bank Entity 수정

--- bank/src/main/java/com/bank/bank/entity/Bank.java
+++ bank/src/main/java/com/bank/bank/entity/Bank.java
@@ -1,18 +1,17 @@
 package com.bank.bank.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "bank")
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Bank {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer bankId;
+    @Id @GeneratedValue
+    @Column(name = "bank_id")
+    private Integer id;
+
+    @Column(name = "bank_name")
+    private String bankName;
 }
