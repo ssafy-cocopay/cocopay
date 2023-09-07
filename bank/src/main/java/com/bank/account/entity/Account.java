4982aed5732151ed59b5bd8b9ea283e5cf15b0e8
HongJiMin <baker0929@naver.com>
2023-09-07 10:19:21 +0900

    feat : 계좌 엔티티 생성

--- bank/src/main/java/com/bank/account/entity/Account.java
+++ bank/src/main/java/com/bank/account/entity/Account.java
@@ -1,13 +1,47 @@
 package com.bank.account.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.bank.bank.entity.Bank;
+import com.bank.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "account")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Account {
-    @Id @GeneratedValue
-    private Integer id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_id")
+    private String id;
+
+    @Column(name = "account_num", length = 19, nullable = false)
+    private String num;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private int balance;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime registedDate = LocalDateTime.now();
+
+    private LocalDateTime withdrawDate;
+
+    //======= 매핑 =======
+    @ManyToOne
+    @JoinColumn(name = "uuid")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name="bank_id")
+    private Bank bank;
+
 }
