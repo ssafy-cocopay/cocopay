da42b46216af46087cff0d1c8904bece68baef7e
hwanhee <dup07168@naver.com>
2023-09-07 16:46:04 +0900

    feat : account RepositoryCustom 생성

--- bank/src/main/java/com/bank/account/repository/AccountRepositoryCustom.java
+++ bank/src/main/java/com/bank/account/repository/AccountRepositoryCustom.java
@@ -2,8 +2,9 @@ package com.bank.account.repository;
 
 import com.bank.account.entity.Account;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepositoryCustom {
-    public Optional<Account> findAccount(Integer uuid, Integer bankId);
+    List<Account> findAccount(Integer id, Integer uuid, Integer bankId);
 }
