cef98c517c9607c14b3722a0f7f607590995ff4e
hwanhee <dup07168@naver.com>
2023-09-07 16:45:50 +0900

    feat : account Repository 생성

--- bank/src/main/java/com/bank/account/repository/AccountRepository.java
+++ bank/src/main/java/com/bank/account/repository/AccountRepository.java
@@ -3,6 +3,6 @@ package com.bank.account.repository;
 import com.bank.account.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountRepository extends JpaRepository<Account,Integer> {
+public interface AccountRepository extends JpaRepository<Account,Integer>, AccountRepositoryCustom {
 
 }
