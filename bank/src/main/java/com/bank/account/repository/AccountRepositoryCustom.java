89fc062136d7df0d41273df1bca0a5be8ada16de
hwanhee <dup07168@naver.com>
2023-09-08 17:43:44 +0900

    refactor : AccountRepositoryCustom 안쓰는 import 삭제

--- bank/src/main/java/com/bank/account/repository/AccountRepositoryCustom.java
+++ bank/src/main/java/com/bank/account/repository/AccountRepositoryCustom.java
@@ -3,8 +3,7 @@ package com.bank.account.repository;
 import com.bank.account.entity.Account;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface AccountRepositoryCustom {
-    List<Account> findAccount(Integer id, Integer uuid, Integer bankId);
+    List<Account> findAccount(Integer id, Integer uuid, Integer bankId, String accountNum);
 }
