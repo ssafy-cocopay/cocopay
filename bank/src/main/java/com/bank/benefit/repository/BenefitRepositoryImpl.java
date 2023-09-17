c8fcb1862deaa677c54d4597fba3c8acd9bf8a7f
한성현 <hsh5349@kakao.com>
2023-09-17 14:12:51 +0900

    feat : 유저가 보유한 카드의 특정 혜택 리스트 조회 쿼리 생성

--- bank/src/main/java/com/bank/benefit/repository/BenefitRepositoryImpl.java
+++ bank/src/main/java/com/bank/benefit/repository/BenefitRepositoryImpl.java
@@ -1,9 +1,10 @@
 package com.bank.benefit.repository;
 
+import com.bank.benefit.dto.BenefitInfoResponseDto;
 import com.bank.benefit.entity.Benefit;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
-
 
 import java.util.List;
 
@@ -13,8 +14,10 @@ import static com.bank.card.entity.QUserCard.userCard;
 
 
 @RequiredArgsConstructor
-public class BenefitRepositoryImpl implements BenefitRepositoryCustom{
+public class BenefitRepositoryImpl implements BenefitRepositoryCustom {
+
     private final JPAQueryFactory jpaQueryFactory;
+
     @Override
     public List<Benefit> getBenefitListByCardUid(Integer cardUuid) {
         return jpaQueryFactory
@@ -22,6 +25,25 @@ public class BenefitRepositoryImpl implements BenefitRepositoryCustom{
                 .leftJoin(benefit.card, card)
                 .leftJoin(card.userCards, userCard)
                 .on(userCard.id.eq(cardUuid))
+                .fetch();
+    }
+
+    @Override
+    public List<BenefitInfoResponseDto> findBenefitByCardList(List<Integer> cardUuidList, String category, String storeName) {
+
+        return jpaQueryFactory
+                .select(Projections.fields(BenefitInfoResponseDto.class,
+                        benefit.category,
+                        benefit.storeName,
+                        benefit.limit,
+                        benefit.discount,
+                        userCard.id.as("cardUuid")))
+                .from(benefit)
+                .join(benefit.card, card)
+                .join(card.userCards, userCard)
+                .on(userCard.id.in(cardUuidList),
+                        benefit.category.eq(category),
+                        benefit.storeName.eq(storeName))
                 .fetch();
     }
 }
