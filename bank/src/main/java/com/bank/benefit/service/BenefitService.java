e96ab1b0f91fd0b24da3e210b2b1c8f30d5dfd83
한성현 <hsh5349@kakao.com>
2023-09-17 14:13:11 +0900

    feat : 사용자가 보유한 카드들의 특정 혜택 조회 서비스 생성

--- bank/src/main/java/com/bank/benefit/service/BenefitService.java
+++ bank/src/main/java/com/bank/benefit/service/BenefitService.java
@@ -1,5 +1,6 @@
 package com.bank.benefit.service;
 
+import com.bank.benefit.dto.BenefitInfoResponseDto;
 import com.bank.benefit.entity.Benefit;
 import com.bank.benefit.repository.BenefitRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,5 +15,10 @@ public class BenefitService {
 
     public List<Benefit> getBenefitList(Integer cardUuid) {
         return benefitRepository.getBenefitListByCardUid(cardUuid);
+    }
+
+    //사용자가 보유한 카드들의 특정 혜택 조회
+    public List<BenefitInfoResponseDto> findBenefitList(List<Integer> cardUuidList, String category, String storeName) {
+        return benefitRepository.findBenefitByCardList(cardUuidList, category, storeName);
     }
 }
