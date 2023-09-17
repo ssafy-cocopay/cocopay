3d5a6adf9d8b5f2647ee9660a92e73b77d9ba49d
hellobaemin <hellobaemin@gmail.com>
2023-09-17 15:29:11 +0900

    fix : React Native Babel ESLint 에러 해결

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+function WebApp() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        paddingTop: "100px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <div className="App">COCOPAY</div>
+      <button style={{ width: "100px", marginTop: "30px" }}>인증하기</button>
     </div>
   );
 }
 
-export default App;
+export default WebApp;
