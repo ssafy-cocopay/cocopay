5388abc94025d8183bef8e61ed81e0069277049a
한성현 <hsh5349@kakao.com>
2023-09-17 15:58:04 +0900

    test : 유저, 은행 더미 생성 메서드 추가

---
+++ bank/src/test/java/com/bank/DummyDataTest.java
@@ -0,0 +1,76 @@
+package com.bank;
+
+import com.bank.bank.entity.Bank;
+import com.bank.bank.repository.BankRepository;
+import com.bank.user.entity.User;
+import com.bank.user.service.UserService;
+import com.github.javafaker.Faker;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+
+@SpringBootTest
+public class DummyDataTest {
+
+    JPAQueryFactory jpaQueryFactory;
+
+    Faker faker = new Faker(new Locale("ko"));
+
+    //휴대폰 번호 정규식
+    String pattern = "^01[0-9]-\\d{4}-\\d{4}$";
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    BankRepository bankRepository;
+
+    @Test
+    public void userDummy() {
+
+        for (int i = 0; i < 10; i++) {
+            User user = new User();
+
+            String name = faker.name().fullName().replace(" ", "");
+            String email = faker.bothify("???####@gmail.com");
+            String tel = faker.bothify("010-####-####");
+            String password = faker.internet().password(4, 13);
+
+            user.setName(name);
+            user.setTel(tel);
+            user.setPassword(password);
+            user.setEmail(email);
+
+            userService.registUser(user);
+        }
+    }
+
+    @Test
+    public void bankDummy() {
+
+        List<String> nameList = new ArrayList<>();
+        //여기에 넣고 싶은 금융권 이름
+        nameList.add("농협");
+        nameList.add("우리");
+        nameList.add("신한");
+        nameList.add("국민");
+        nameList.add("카카오뱅크");
+
+        List<Bank> bankList = nameList.stream()
+                .map(name -> {
+                    Bank bank = new Bank();
+                    bank.setBankName(name);
+
+                    return bank;
+                }).toList();
+
+        bankRepository.saveAll(bankList);
+    }
+}
