763c193a53c417b8cb6b38b160fe8819d3928d9e
seol <seolhyehyeon@gmail.com>
2023-09-19 17:51:44 +0900

    fix :라우팅children 수정

--- front/src/router/routes.tsx
+++ front/src/router/routes.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import App from "@/App";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { PATH } from "@/constants/path";
-import SignupPage from "@/pages/SignupPage/SignupPage"
+import SignupPage from "@/pages/SignupPage/SignupPage";
 import NotFoundPage from "@/pages/NotFoundPage/NotFoundPage";
 import PasswordSettingPage from "@/pages/PasswordSettingPage/PasswordSettingPage";
 import FingerSettingPage from "@/pages/FingerSettingPage/FingerSettingPage";
@@ -23,95 +23,123 @@ import MyPage from "@/pages/Mypage/MyPage";
 import MypageWithdrawalPage from "@/pages/MyPageWithdrawalPage/MyPageWithdrawalPage";
 import PayOnlinePage from "@/pages/PayOnlinePage/PayOnlinePage";
 import PayOnlineCompletePage from "@/pages/PayOnlineCompletePage/PayOnlineCompletePage";
+import MainPage from "@/pages/MainPage/MainPage";
 
 const router = createBrowserRouter([
   {
     path: PATH.ROOT,
     element: <App />,
-    errorElement: <NotFoundPage />
+    errorElement: <NotFoundPage />,
   },
   {
     path: PATH.SIGNUP,
-    element: <SignupPage />
+    element: <SignupPage />,
   },
   {
     path: PATH.PASSWORD_SETTING,
-    element: <PasswordSettingPage />
+    element: <PasswordSettingPage />,
   },
   {
     path: PATH.FIGNER_SETTING,
-    element: <FingerSettingPage />
+    element: <FingerSettingPage />,
   },
   {
     path: PATH.SPLASH,
-    element: <SplashPage />
+    element: <SplashPage />,
   },
   {
     path: PATH.CARDUPLOAD,
-    element: <CardUploadPage />
+    element: <CardUploadPage />,
   },
   {
     path: PATH.PRIORITY,
-    element: <PriorityPage />
+    element: <PriorityPage />,
   },
   {
     path: PATH.LOGIN_FINGER,
-    element: <LoginFingerPage />
+    element: <LoginFingerPage />,
   },
   {
     path: PATH.LOGIN_PASSWORD,
-    element: <LoginPasswordPage />
+    element: <LoginPasswordPage />,
+  },
+  {
+    path: PATH.MAIN,
+    element: <MainPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: PATH.CARDLIST,
+        element: <CardListPage />,
+        children: [
+          { index: true, element: <CardDetailPage /> },
+          {
+            path: PATH.SCAN_CARDINFO,
+            element: <ScanCardInfoPage />,
+          },
+          {
+            path: PATH.CARD_DETAIL_PURCHASED,
+            element: <CardDetailPurchasedPage />,
+          },
+        ],
+      },
+      {
+        path: PATH.STATISTICS,
+        element: <StatisticsPage />,
+      },
+      {
+        path: PATH.MYPAGE,
+        element: <MyPage />,
+      },
+    ],
   },
   {
     path: PATH.HOME,
     element: <HomePage />,
-    children: [
-      
-    ]
   },
   {
     path: PATH.PAYOFFLINE,
-    element: <PayOfflinePage />
+    element: <PayOfflinePage />,
   },
-  {
-    path: PATH.CARDLIST,
-    element: <CardListPage />
-  },
+  // {
+  //   path: PATH.CARDLIST,
+  //   element: <CardListPage />,
+  // },
   {
     path: PATH.SCAN,
-    element: <ScanPage />
+    element: <ScanPage />,
   },
-  {
-    path: PATH.SCAN_CARDINFO,
-    element: <ScanCardInfoPage />
-  },
-  {
-    path: PATH.CARD_DETAIL,
-    element: <CardDetailPage />
-  },
-  {
-    path: PATH.CARD_DETAIL_PURCHASED,
-    element: <CardDetailPurchasedPage />
-  },
-  {
-    path: PATH.STATISTICS,
-    element: <StatisticsPage />
-  },
-  {
-    path: PATH.MYPAGE,
-    element: <MyPage />
-  },
+  // {
+  //   path: PATH.SCAN_CARDINFO,
+  //   element: <ScanCardInfoPage />,
+  // },
+  // {
+  //   path: PATH.CARD_DETAIL,
+  //   element: <CardDetailPage />,
+  // },
+  // {
+  //   path: PATH.CARD_DETAIL_PURCHASED,
+  //   element: <CardDetailPurchasedPage />,
+  // },
+  // {
+  //   path: PATH.STATISTICS,
+  //   element: <StatisticsPage />,
+  // },
+  // {
+  //   path: PATH.MYPAGE,
+  //   element: <MyPage />,
+  // },
   {
     path: PATH.MYPAGE_WITHDRAWAL,
-    element: <MypageWithdrawalPage />
+    element: <MypageWithdrawalPage />,
   },
   {
     path: PATH.PAYONLINE,
-    element: <PayOnlinePage />
+    element: <PayOnlinePage />,
   },
   {
     path: PATH.PAYONLINECOMPLETE,
-    element: <PayOnlineCompletePage />
+    element: <PayOnlineCompletePage />,
   },
 ]);
 
