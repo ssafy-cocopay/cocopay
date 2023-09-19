929c7a220702dd86c9a57e28ec6fbd555a273781
한성현 <hsh5349@kakao.com>
2023-09-19 15:11:02 +0900

    test : url 접두사 변경

--- bank/src/main/java/com/bank/TestController.java
+++ bank/src/main/java/com/bank/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class TestController {
 
-    @GetMapping
+    @GetMapping("/test")
     public String test() {
         return "hello";
     }
