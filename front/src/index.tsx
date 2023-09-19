faa54dee39851f93604579d1877708deff09663c
“배민경” <hellobaemin@gmail.com>
2023-09-19 15:05:52 +0900

    feat : index.css 앱 라우터 지정 및 리코일, 스타일드 컴포넌트 설정

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,15 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import AppRouter from "./router/routes";
+import { RecoilRoot } from "recoil";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "../styles/GlobalStyle";
+import theme from "../styles/theme";
+import "@/styles/font.css";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      suspense: true,
+    },
+  },
+});
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <ThemeProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <GlobalStyle />
+          <AppRouter />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
 
