1f0bda724776d432a23708cfbf90f0c3bbabcc10
HongJiMin <baker0929@naver.com>
2023-09-19 15:36:38 +0900

    [S09P22B208-167] refactor : 카드 히스토리 엔티티 내 Builder 추가

--- bank/src/main/java/com/bank/card_history/entity/CardHistory.java
+++ bank/src/main/java/com/bank/card_history/entity/CardHistory.java
@@ -3,6 +3,7 @@ package com.bank.card_history.entity;
 import com.bank.card.entity.UserCard;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CardHistory {
 
     @Id
