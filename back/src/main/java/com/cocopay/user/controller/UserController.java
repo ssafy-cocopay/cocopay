22aa8c48b6b5ac32b7321202c6546f74ba2a4c98
HongJiMin <baker0929@naver.com>
2023-09-20 17:37:40 +0900

    [S09P22B208-175] feat : 유저 인증 관련 컨트롤러 추가

--- back/src/main/java/com/cocopay/user/controller/UserController.java
+++ back/src/main/java/com/cocopay/user/controller/UserController.java
@@ -1,11 +1,46 @@
 package com.cocopay.user.controller;
 
+import com.cocopay.user.dto.AuthRequestDto;
+import com.cocopay.user.dto.UserJoinDto;
+import com.cocopay.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/message-auth")
+    public ResponseEntity sendAuthMessage(@RequestBody AuthRequestDto authRequestDto)
+    {
+        String code = userService.sendRandomMessage(authRequestDto.getTel());
+
+        //redis에 code 저장
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/join")
+    public ResponseEntity<?> join(@RequestBody UserJoinDto userJoinDto)
+    {
+
+        userService.join(userJoinDto);
+        return null;
+    }
+
+    @PostMapping("/login/password")
+    public ResponseEntity<?> loginByPassword()
+    {
+        return null;
+    }
+
+    @PostMapping("/login/bio")
+    public ResponseEntity<?> loginByBio()
+    {
+        return null;
+    }
 }
