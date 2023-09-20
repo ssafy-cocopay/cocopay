c6f9e71105fca7255fa1e5e077250e876a5946a2
한성현 <hsh5349@kakao.com>
2023-09-20 21:04:46 +0900

    [S09P22B208-176] feat : RedisMapper 생성

---
+++ back/src/main/java/com/cocopay/redis/mapper/RedisMapper.java
@@ -0,0 +1,16 @@
+package com.cocopay.redis.mapper;
+
+import com.cocopay.payment.dto.res.PerformanceResponseDto;
+import com.cocopay.redis.key.PerformanceKey;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface RedisMapper {
+
+    PerformanceKey toPerformanceKey(PerformanceResponseDto dto);
+
+    List<PerformanceKey> toPerformanceKeyList(List<PerformanceResponseDto> dtoList);
+}
+경
