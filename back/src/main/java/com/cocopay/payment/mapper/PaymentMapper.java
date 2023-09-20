3e19efc8422aa278688d05f75d41f20bd8c2e7a3
한성현 <hsh5349@kakao.com>
2023-09-21 01:57:03 +0900

    [S09P22B208-176] refactor : 매핑 방식 변경

--- back/src/main/java/com/cocopay/payment/mapper/PaymentMapper.java
+++ back/src/main/java/com/cocopay/payment/mapper/PaymentMapper.java
@@ -1,6 +1,7 @@
 package com.cocopay.payment.mapper;
 
 import com.cocopay.payment.dto.res.CardOfferResponseDto;
+import com.cocopay.payment.dto.res.CardResponseDto;
 import com.cocopay.redis.key.PerformanceKey;
 import com.cocopay.usercard.entity.UserCard;
 import org.mapstruct.Mapper;
@@ -9,24 +10,19 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface PaymentMapper {
 
-    @Mapping(source = "userCard.cardUuid", target = "cardUuid")
-    @Mapping(source = "userCard.serialNumber", target = "serialNumber")
-    @Mapping(source = "userCard.cardName",target = "cardName")
-    @Mapping(source = "userCard.visa",target = "visa")
-    @Mapping(source = "userCard.master",target = "master")
-    @Mapping(source = "userCard.cardDefaultImage", target = "cardDefaultImage")
-    @Mapping(source = "key.level",target = "level")
-    @Mapping(source = "key.nextLevel",target = "nextLevel")
-    @Mapping(source = "key.price",target = "price")
-    @Mapping(source = "key.totalPrice",target = "totalPrice")
-    @Mapping(source = "userCard.cardOrder",target = "cardOrder")
-    CardOfferResponseDto toResponseDto(PerformanceKey key, UserCard userCard);
+//    @Mapping(source = "userCard.cardUuid", target = "cardUuid")
+//    @Mapping(source = "userCard.serialNumber", target = "serialNumber")
+//    @Mapping(source = "userCard.cardName",target = "cardName")
+//    @Mapping(source = "userCard.visa",target = "visa")
+//    @Mapping(source = "userCard.master",target = "master")
+//    @Mapping(source = "userCard.cardDefaultImage", target = "cardDefaultImage")
+//    @Mapping(source = "userCard.cardOrder",target = "cardOrder")
+//    @Mapping(source = "key.level",target = "level")
+//    @Mapping(source = "key.nextLevel",target = "nextLevel")
+//    @Mapping(source = "key.price",target = "price")
+//    @Mapping(source = "key.totalPrice",target = "totalPrice")
+    CardOfferResponseDto toResponseDto(PerformanceKey performance, CardResponseDto card,int orderPrice);
 
-    @Mapping(source = "userCard", target = "cardUuid")
-    @Mapping(source = "key.level",target = "level")
-    @Mapping(source = "key.nextLevel",target = "nextLevel")
-    @Mapping(source = "key.price",target = "price")
-    @Mapping(source = "key.totalPrice",target = "totalPrice")
-    CardOfferResponseDto toResponseDtoTest(PerformanceKey key, int userCard);
-
+    @Mapping(source = "id",target = "cardUuid")
+    CardResponseDto toCardResponseDto(UserCard userCard);
 }
