2c8421ba0f466002faafa31d42aece769419c863
한성현 <hsh5349@kakao.com>
2023-09-20 17:32:01 +0900

    [S09P22B208-176] feat : 온라인 결제 api 생성 중

---
+++ back/src/main/java/com/cocopay/payment/dto/req/Test.java
@@ -0,0 +1,14 @@
+package com.cocopay.payment.dto.req;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Test {
+    private List<Integer> cardUuidList;
+}
