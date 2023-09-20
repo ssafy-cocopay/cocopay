96a15ecb6d894fafbc914157c23f524434b0df55
한성현 <hsh5349@kakao.com>
2023-09-20 23:54:43 +0900

    [S09P22B208-176] fix : 코코카드 조회 조건 오류 수정

--- back/src/main/java/com/cocopay/usercard/repository/UserCardRepositoryImpl.java
+++ back/src/main/java/com/cocopay/usercard/repository/UserCardRepositoryImpl.java
@@ -20,7 +20,7 @@ public class UserCardRepositoryImpl implements UserCardRepositoryCustom{
         return jpaQueryFactory
                 .selectFrom(userCard)
                 .where(userCard.user.id.eq(userId),
-                        userCard.cocoType.eq(true))
+                        userCard.cocoType.eq(false))
                 .fetch();
     }
 }
