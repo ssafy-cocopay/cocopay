305596bffa23a62f5da6dd616358a12b1a561447
한성현 <hsh5349@kakao.com>
2023-09-21 00:47:38 +0900

    [S09P22B208-176] feat : 온라인 결제 제네릭 클래스 생성

---
+++ back/src/main/java/com/cocopay/payment/dto/res/OnlineResponse.java
@@ -0,0 +1,15 @@
+package com.cocopay.payment.dto.res;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OnlineResponse<T> {
+
+    private T data;
+
+    private int totalPrice;
+}
