ae44aa36beb2570e708a71a5fcfe1a67f6e6f785
김서영 <qkdxks0922@gmail.com>
2023-09-20 16:04:34 +0900

    feat : button으로 화면 이동 라우팅 완료

--- front/src/router/routes.tsx
+++ front/src/router/routes.tsx
@@ -71,21 +71,22 @@ const router = createBrowserRouter([
       {
         path: PATH.CARDLIST,
         element: <CardListPage />,
-        children: [
-          { index: true, element: <CardDetailPage /> },
-          {
-            path: PATH.SCAN_CARDINFO,
-            element: <ScanCardInfoPage />,
-          },
-          {
-            path: PATH.CARD_DETAIL_PURCHASED,
-            element: <CardDetailPurchasedPage />,
-          },
-        ],
+      },
+      {
+        path: PATH.SCAN_CARDINFO,
+        element: <ScanCardInfoPage />,
       },
       {
         path: PATH.STATISTICS,
         element: <StatisticsPage />,
+      },
+      {
+        path: PATH.CARD_DETAIL,
+        element: <CardDetailPage />,
+      },
+      {
+        path: PATH.CARD_DETAIL_PURCHASED,
+        element: <CardDetailPurchasedPage />,
       },
       {
         path: PATH.MYPAGE,
