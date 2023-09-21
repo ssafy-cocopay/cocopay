e370a3d75d19f7865e44c8820b7c7633c92c6b5c
seolhyehyeon <seolhyehyeon@gmail.com>
2023-09-21 23:12:49 +0900

    fix : 컨테이너 import

--- front/src/pages/SignupPage/SignupPage.tsx
+++ front/src/pages/SignupPage/SignupPage.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import {} from "./SignupPage.styles";
 import Button from "@/components/atoms/Button/Button";
-
+import { Container } from "@/components/atoms/Container/Container.styles";
 import { PATH } from "@/constants/path";
 import { useForm, SubmitHandler } from "react-hook-form";
 
@@ -28,64 +28,82 @@ const SignupPage = () => {
   const onSubmitHandler: SubmitHandler<FormValue> = (data) => {
     console.log(data);
   };
+
   return (
-    <form onSubmit={handleSubmit(onSubmitHandler)}>
-      <div>
-        <label>이름</label>
-        <input
-          id="name"
-          type="text"
-          placeholder="이름을 작성해주세요"
-          // input의 기본 config를 작성
-          {...register("name", {
-            required: true,
-            minLength: { value: 2, message: "최소 2자 이상 입력하세요" },
-            maxLength: { value: 8, message: "최대 8자 까지 가능합니다." },
-          })}
-        />
-        {errors.name && <small role="alert">{errors.name.message}</small>}
+    <Container>
+      <form onSubmit={handleSubmit(onSubmitHandler)}>
+        <div>
+          <input
+            id="name"
+            type="text"
+            placeholder="이름을 작성해주세요"
+            // input의 기본 config를 작성
+            {...register("name", {
+              required: true,
+              minLength: { value: 2, message: "최소 2자 이상 입력하세요" },
+              maxLength: { value: 8, message: "최대 8자 까지 가능합니다." },
+            })}
+          />
+          {errors.name && <small role="alert">{errors.name.message}</small>}
 
-        <label>주민등록번호</label>
-        <input
-          {...register("identification_number", {
-            required: true,
-            pattern: {
-              value: /^[0-9]{6}$/,
-              message: "주민등록번호 6자리를 입력해주세요!",
-            },
-          })}
-        />
-
-        {errors.identification_number && (
-          <small role="alert">{errors.identification_number.message}</small>
-        )}
-        <label>핸드폰번호</label>
-        <input
-          {...register("phone_number", {
-            required: true,
-            pattern: {
-              value: /^[0-9]{11}$/,
-              message: "'-'없이 번호만 입력해주세요!'",
-            },
-          })}
-        />
-        {errors.phone_number && (
-          <small role="alert">{errors.phone_number.message}</small>
-        )}
-        {/* <button
+          <label>주민등록번호</label>
+          <input
+            {...register("identification_number", {
+              required: true,
+              pattern: {
+                value: /^[0-9]{6}$/,
+                message: "주민등록번호 6자리를 입력해주세요!",
+              },
+            })}
+          />
+          <span>-</span>
+          <input
+            {...register("phone_number", {
+              required: true,
+              maxLength: 1,
+            })}
+          />
+          <span>*****</span>
+          {errors.identification_number && (
+            <small role="alert">{errors.identification_number.message}</small>
+          )}
+          <br />
+          <div>
+            {/* <!-- 드롭다운(싱글타입) --> */}
+            <select name="telecom" id="telecom">
+              <option value="SKT">SKT</option>
+              <option value="KT">KT</option>
+              <option value="U+">LG U+</option>
+            </select>
+          </div>
+          <label>핸드폰번호</label>
+          <input
+            {...register("phone_number", {
+              required: true,
+              pattern: {
+                value: /^[0-9]{11}$/,
+                message: "'-'없이 번호만 입력해주세요!'",
+              },
+            })}
+          />
+          {errors.phone_number && (
+            <small role="alert">{errors.phone_number.message}</small>
+          )}
+          {/* <button
           type="button"
           onClick={() => {
             console.log(1);
           }}
         ></button> */}
-        <Button
-          onClick={() => navigatePage(PATH.PASSWORD_SETTING)}
-          size="medium"
-        >
-          확인
-        </Button>
-      </div>
-    </form>
+          <Button
+            onClick={() => navigatePage(PATH.PASSWORD_SETTING)}
+            size="medium"
+          >
+            확인
+          </Button>
+        </div>
+      </form>
+    </Container>
   );
 };
 export default SignupPage;
