975cd1a55f731a1f50d33e2e1c018256f2305abc
hwanhee <dup07168@naver.com>
2023-09-21 15:25:00 +0900

    refactor : category 타입 변경

--- bank/src/main/java/com/bank/card_history/dto/CategoryDto.java
+++ bank/src/main/java/com/bank/card_history/dto/CategoryDto.java
@@ -1,10 +1,11 @@
 package com.bank.card_history.dto;
 
+import com.bank.card_history.entity.Category;
 import lombok.Getter;
 
 @Getter
 public class CategoryDto {
-    private String category;
+    private Category category;
     private int discountAmount;
     private Long price;
 
