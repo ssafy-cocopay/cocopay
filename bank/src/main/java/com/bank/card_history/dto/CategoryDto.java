7d74fb572e5dd7dc5ad7249d3fac697e9645fad4
hwanhee <dup07168@naver.com>
2023-09-21 15:20:27 +0900

    feat : 카테고리별 값 응답 Dto

---
+++ bank/src/main/java/com/bank/card_history/dto/CategoryDto.java
@@ -0,0 +1,11 @@
+package com.bank.card_history.dto;
+
+import lombok.Getter;
+
+@Getter
+public class CategoryDto {
+    private String category;
+    private int discountAmount;
+    private Long price;
+
+}
