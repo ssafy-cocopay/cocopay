7a62612cada80ed25f57f7ba6130fd3c7cfc594a
“배민경” <hellobaemin@gmail.com>
2023-09-21 10:14:19 +0900

    fix: App.tsx conflict 진짜 해결

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -7,6 +7,7 @@ import { PATH } from "@/constants/path";
 import coco from "@/assets/images/COCO.png";
 import { Image } from "@/components/atoms/Image/Image";
 import { Container } from "@/components/atoms/Container/Container.styles";
+import { Text } from "@/components/atoms/Text/Text.styles";
 import { InputContainer } from "./components/atoms/Input/Input.styles";
 import Input from "./components/atoms/Input/Input";
 
@@ -17,41 +18,14 @@ function App() {
   };
 
   return (
-    <Background
-      colormode="blue"
-      style={{
-        // minHeight: "100vh",
-        paddingTop: "100px",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <div className="App">COCOPAY</div>
-      {/* <InputContainer inputType="text"> */}
-      <Input inputType="text"></Input>
-      <Button
-        onClick={() => navigatePage(PATH.LOGIN_FINGER)}
-        size="medium"
-        $width="200px"
-      >
-        로그인
-      </Button>
-      <Button
-        onClick={() => navigatePage(PATH.SIGNUP)}
-        option="activated"
-        size="medium"
-        $width="200px"
-      >
-        회원가입
-      </Button>
-      <Button option="deActivated" size="medium" $width="200px">
-        확인
-      </Button>
-      <Button option="danger" size="medium" $width="200px">
-        활성화
-      </Button>
-      <button style={{ width: "100px", marginTop: "30px" }}>인증하기</button>
+    <Background colormode="blue">
+      <Container>
+        <Image src={coco} width={10} style={{}} />
+        <Button size="large">로그인</Button>
+        <Button size="large" option="activated">
+          회원가입
+        </Button>
+      </Container>
     </Background>
   );
 }
