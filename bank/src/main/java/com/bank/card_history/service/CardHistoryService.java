d6d02a4cccdaec89f60c8200c97626b72f828f41
hwanhee <dup07168@naver.com>
2023-09-21 15:13:44 +0900

    feat : 사용자 아이디로 한달 사용 금액 및 혜택 조회

--- bank/src/main/java/com/bank/card_history/service/CardHistoryService.java
+++ bank/src/main/java/com/bank/card_history/service/CardHistoryService.java
@@ -1,11 +1,17 @@
 package com.bank.card_history.service;
 
+import com.bank.card.entity.UserCard;
+import com.bank.card.repository.usercard.UserCardRepository;
+import com.bank.card_history.dto.CategoryDto;
+import com.bank.card_history.dto.CategoryResponseDto;
+import com.bank.card_history.dto.FindHistoryByUserId;
 import com.bank.card_history.dto.HistoryFindDto;
 import com.bank.card_history.entity.CardHistory;
 import com.bank.card_history.repository.CardHistoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,8 +19,34 @@ import java.util.List;
 public class CardHistoryService {
 
     private final CardHistoryRepository cardHistoryRepository;
+    private final UserCardRepository userCardRepository;
 
     public List<CardHistory> getCardHistory(HistoryFindDto historyFindDto) {
         return cardHistoryRepository.getCardHistory(historyFindDto);
+    }
+
+    public CategoryResponseDto getAllamount(FindHistoryByUserId findHistoryByUserId){
+        //1.사용자의 모든 카드 조회
+        List<UserCard> userCardList = userCardRepository.findUserCardByUUID(findHistoryByUserId.getUserId());
+        List<Integer> userCardIdList = new ArrayList<>();
+        for (UserCard userCard : userCardList) {
+            userCardIdList.add(userCard.getId());
+        }
+        //2.조회한 모든 카드들을 카테고리별로 조회
+        List<CategoryDto> categoryDtoList = cardHistoryRepository.getCardHistoryByCategory(userCardIdList,findHistoryByUserId.getStartDate(),findHistoryByUserId.getEndDate());
+        int allDiscountAmount = 0;
+        Long allPrice = 0L;
+        for (CategoryDto categoryDto:categoryDtoList) {
+            allPrice += categoryDto.getPrice();
+            allDiscountAmount += categoryDto.getDiscountAmount();
+        }
+        CategoryResponseDto categoryResponseDto = CategoryResponseDto.builder()
+                .categoryDtoList(categoryDtoList)
+                .allPrice(allPrice)
+                .allDiscountAmount(allDiscountAmount)
+                .build();
+
+        return categoryResponseDto;
+
     }
 }
