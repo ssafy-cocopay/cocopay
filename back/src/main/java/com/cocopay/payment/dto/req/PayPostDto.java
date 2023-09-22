af41b500a1815e1cc5c79cfd8cc2c748830bd7cf
한성현 <hsh5349@kakao.com>
2023-09-23 01:52:49 +0900

    refactor : 결제 요청값 cardId 추가

--- back/src/main/java/com/cocopay/payment/dto/req/PayPostDto.java
+++ back/src/main/java/com/cocopay/payment/dto/req/PayPostDto.java
@@ -5,8 +5,10 @@ import lombok.Getter;
 
 @Getter
 @Builder
-public class OnlinePayPostDto {
+public class PayPostDto {
     private int userId;
+
+    private Integer cardId;
 
     private String category;
 
