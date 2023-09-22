2085c256ba785e4809bd316a6b9970a8d00056a4
seol <seolhyehyeon@gmail.com>
2023-09-22 14:15:10 +0900

    fix : 회원가입 기본틀 설정(드롭다운제외)

--- front/src/pages/SignupPage/SignupPage.tsx
+++ front/src/pages/SignupPage/SignupPage.tsx
@@ -39,21 +39,51 @@ const SignupPage = () => {
         <Container border={true} padding="none" left={true}>
           <Text size="subtitle1">
             코코페이 이용을 위해
-            <br /> <b>본인확인</b>을 진행해주세요.
+            <br /> <b>본인확인</b>을 진행해주세요
           </Text>
+          <br />
           <Input placeholder="이름을 작성해주세요"></Input>
-          <Wrapper
-            flexDirection="row"
-            alignItems="flex-start"
-            justifyContent="space-between"
-          >
-            <Input placeholder="주민등록번호 앞자리" width={122}></Input>
-            <Text size="subtitle1">-</Text>
+          <br />
+          <Wrapper flexDirection="row" justifyContent="space-between">
+            <Input
+              {...register("identification_number", {
+                pattern: {
+                  value: /^[0-9]{13}$/, // 13자리 숫자인지 확인하는 정규식
+                  message: "주민등록번호를 정확히 입력하세요 (숫자 13자리)",
+                },
+              })}
+              width={122}
+            ></Input>
+            <Text size="subtitle1" color="grey3">
+              -
+            </Text>
             <Input width={35} textAlign="center" paddingLeft={0}></Input>
-            <Image src={Dots} width={30} height={15} $unit="%"></Image>
+            <Image src={Dots} width={40} $unit="%"></Image>
           </Wrapper>
           <br />
-
+          <Input placeholder="핸드폰번호"></Input>
+          <br />
+          <Wrapper flexDirection="raw" justifyContent="space-between">
+            <Input width={150}></Input>
+            <Button
+              onClick={() => console.log("인증번호전송")}
+              $fontSize="18px"
+              option="activated"
+              $width="50%"
+              $borderRadius="10px"
+            >
+              인증번호 받기
+            </Button>
+          </Wrapper>
+          <br />
+          <Button
+            option="deActivated"
+            $borderRadius="10px"
+            onClick={() => navigatePage(PATH.PASSWORD_SETTING)}
+            size="medium"
+          >
+            확인
+          </Button>
           {/* <input
             id="name"
             type="text"
@@ -66,8 +96,7 @@ const SignupPage = () => {
             })}
           />
           {errors.name && <small role="alert">{errors.name.message}</small>} */}
-
-          <label>주민등록번호</label>
+          {/* <label>주민등록번호</label>
           <input
             {...register("identification_number", {
               required: true,
@@ -87,7 +116,7 @@ const SignupPage = () => {
           <span>*****</span>
           {errors.identification_number && (
             <small role="alert">{errors.identification_number.message}</small>
-          )}
+          )} */}
           <br />
           <div>
             {/* <!-- 드롭다운(싱글타입) --> */}
@@ -110,18 +139,6 @@ const SignupPage = () => {
           {errors.phone_number && (
             <small role="alert">{errors.phone_number.message}</small>
           )}
-          {/* <button
-          type="button"
-          onClick={() => {
-            console.log(1);
-          }}
-        ></button> */}
-          <Button
-            onClick={() => navigatePage(PATH.PASSWORD_SETTING)}
-            size="medium"
-          >
-            확인
-          </Button>
         </Container>
       </form>
     </Container>
