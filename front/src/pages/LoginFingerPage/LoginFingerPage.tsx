43341a9c1a393fefe763a6aba3171ca9d018fa05
“배민경” <hellobaemin@gmail.com>
2023-09-22 12:32:24 +0900

    feat : LoginFingerPage 네비게이팅 to 간편비밀번호

--- front/src/pages/LoginFingerPage/LoginFingerPage.tsx
+++ front/src/pages/LoginFingerPage/LoginFingerPage.tsx
@@ -6,12 +6,18 @@ import { Text } from "@/components/atoms/Text/Text.styles";
 import { Image } from "@/components/atoms/Image/Image";
 import fingerPrint from "@/assets/images/img-fingerprint-blue.png";
 import Button from "@/components/atoms/Button/Button";
+import { useNavigate } from "react-router-dom";
+import { PATH } from "@/constants/path";
 
 const LoginFingerPage = () => {
+  const navigate = useNavigate();
+  const navigatePage = (path: string) => {
+    navigate(path);
+  };
   //TODO: 지문 등록 버튼 눌렀을 때 App에서 지문인증 실행
   return (
     <Background colormode="gradient">
-      <Container border={true} left={true} paddingTop="36px">
+      <Container left={true} paddingTop="36px">
         <Back>뒤로가기</Back>
         {/* 이 WhiteWrapper가 자주 쓰인다면 별도 옵션으로 둬도 되겠다 */}
         <Container
@@ -29,7 +35,11 @@ const LoginFingerPage = () => {
           </Text>
           <Image src={fingerPrint} width={9} margin="30px 0 50px 0"></Image>
           <Button option="activated">지문 인증하기</Button>
-          <Button $border="none" style={{ marginBottom: "20px" }}>
+          <Button
+            $border="none"
+            style={{ marginBottom: "20px" }}
+            onClick={() => navigatePage(PATH.LOGIN_PASSWORD)}
+          >
             건너뛰기
           </Button>
         </Container>
