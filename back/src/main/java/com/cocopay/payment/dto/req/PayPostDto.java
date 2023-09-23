c47c4831b2ae99eb044768a8e5f20b7060c6fca8
한성현 <hsh5349@kakao.com>
2023-09-23 22:24:57 +0900

    feat : 일시불, 할부 결제 타입 추가, 할부 개월 수 추가

--- back/src/main/java/com/cocopay/payment/dto/req/PayPostDto.java
+++ back/src/main/java/com/cocopay/payment/dto/req/PayPostDto.java
@@ -2,17 +2,23 @@ package com.cocopay.payment.dto.req;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 @Builder
 public class PayPostDto {
     private int userId;
 
-    private Integer cardId;
+    private int cardId;
 
     private String category;
 
     private String storeName;
 
     private int orderPrice;
+
+    private String transactionType;
+
+    private Integer installmentMonth;
 }
