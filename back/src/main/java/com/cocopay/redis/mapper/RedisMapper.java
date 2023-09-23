ede7bcbf2ec2c073de575896395ce660b95c1cea
한성현 <hsh5349@kakao.com>
2023-09-23 22:25:15 +0900

    refactor : 파라미터 변경

--- back/src/main/java/com/cocopay/redis/mapper/RedisMapper.java
+++ back/src/main/java/com/cocopay/redis/mapper/RedisMapper.java
@@ -1,11 +1,13 @@
 package com.cocopay.redis.mapper;
 
 import com.cocopay.payment.apicall.dto.res.UserCardBenefitInfoResponseDto;
+import com.cocopay.payment.dto.req.PayPostDto;
 import com.cocopay.payment.dto.res.PerformanceResDto;
 import com.cocopay.redis.key.BenefitKey;
 import com.cocopay.redis.key.OrderKey;
 import com.cocopay.redis.key.PerformanceKey;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -16,7 +18,7 @@ public interface RedisMapper {
 
     List<PerformanceKey> toPerformanceKeyList(List<PerformanceResDto> dtoList);
 
-    OrderKey toOrderKey(int userId, String category, String storeName, int orderPrice);
+    OrderKey toOrderKey(PayPostDto dto);
 
     BenefitKey toBenefitKey(UserCardBenefitInfoResponseDto dto);
 
