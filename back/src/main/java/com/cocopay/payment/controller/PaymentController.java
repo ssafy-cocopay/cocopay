ee6237bbae8a6d88da142c9423ee1af5166df7cf
한성현 <hsh5349@kakao.com>
2023-09-23 22:23:07 +0900

    refactor : 온,오프 api 분기

--- back/src/main/java/com/cocopay/payment/controller/PaymentController.java
+++ back/src/main/java/com/cocopay/payment/controller/PaymentController.java
@@ -1,16 +1,19 @@
 package com.cocopay.payment.controller;
 
 import com.cocopay.payment.dto.req.PayPostDto;
-import com.cocopay.payment.dto.req.PickDto;
+import com.cocopay.payment.dto.req.FinalPayReqDto;
+import com.cocopay.payment.dto.res.CardOfferResDto;
 import com.cocopay.payment.dto.res.OnlineResponse;
 import com.cocopay.payment.service.PaymentService;
+import com.cocopay.redis.service.OrderKeyService;
+import com.cocopay.usercard.entity.UserCard;
+import com.cocopay.usercard.service.UserCardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,18 +21,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/pay")
 public class PaymentController {
     private final PaymentService paymentService;
+    private final OrderKeyService orderKeyService;
+    private final UserCardService userCardService;
 
     @PostMapping("/online")
     public ResponseEntity onlinePay(@RequestBody PayPostDto payPostDto) {
+        orderKeyService.orderKeySave(payPostDto);
 
-        OnlineResponse<?> onlineResponse = paymentService.autoOrDirect(payPostDto);
+        List<CardOfferResDto> cardOfferResDtos = paymentService.autoChanging(payPostDto.getUserId(),
+                payPostDto.getCategory(),
+                payPostDto.getStoreName(),
+                payPostDto.getOrderPrice());
+
+        OnlineResponse<List<CardOfferResDto>> onlineResponse = new OnlineResponse<>(cardOfferResDtos);
 
         return ResponseEntity.ok(onlineResponse);
     }
 
-    @PostMapping("/online/pick")
-    public ResponseEntity cardPick(@RequestBody PickDto pickDto) {
-        paymentService.cardPick(pickDto);
-        return ResponseEntity.ok("결제 요청 완료!!");
+    @PostMapping("/offline/{card-id}")
+    public ResponseEntity offlinePay(@PathVariable ("card-id") int cardId,
+                                     @RequestBody PayPostDto payPostDto) {
+        payPostDto.setCardId(cardId);
+        orderKeyService.orderKeySave(payPostDto);
+
+        paymentService.autoOrDirect(payPostDto);
+        return null;
+    }
+
+    @PostMapping()
+    public ResponseEntity reqPay(@RequestBody FinalPayReqDto finalPayReqDto) {
+        UserCard findUserCard = userCardService.findUserCardById(finalPayReqDto.getCardId());
+
+        //최종 결제 진행
+        paymentService.reqPay(finalPayReqDto,null, findUserCard.getCardUuid());
+
+        return ResponseEntity.ok("결제 요청 완료");
     }
 }
