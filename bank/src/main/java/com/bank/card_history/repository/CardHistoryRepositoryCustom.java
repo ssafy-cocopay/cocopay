ed24ffe63ccfafd4654110a44fe602a262f50d79
한성현 <hsh5349@kakao.com>
2023-09-25 00:11:56 +0900

    feat : 쿼리 생성

--- bank/src/main/java/com/bank/card_history/repository/CardHistoryRepositoryCustom.java
+++ bank/src/main/java/com/bank/card_history/repository/CardHistoryRepositoryCustom.java
@@ -2,6 +2,7 @@ package com.bank.card_history.repository;
 
 import com.bank.card_history.dto.CategoryDto;
 import com.bank.card_history.dto.HistoryFindDto;
+import com.bank.card_history.dto.TotalByMonth;
 import com.bank.card_history.entity.CardHistory;
 
 import java.time.LocalDateTime;
@@ -11,4 +12,6 @@ public interface CardHistoryRepositoryCustom {
     List<CardHistory> getCardHistory(HistoryFindDto historyFindDto);
 
     List<CategoryDto> getCardHistoryByCategory(List<Integer> userCardList, LocalDateTime startDate,LocalDateTime endDate);
+
+    TotalByMonth getTotalByMonth(List<Integer> cardUuidList, int month);
 }
--- bank/src/main/java/com/bank/card_history/repository/CardHistoryRepositoryImpl.java
+++ bank/src/main/java/com/bank/card_history/repository/CardHistoryRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.bank.card_history.repository;
 
 import com.bank.card_history.dto.CategoryDto;
 import com.bank.card_history.dto.HistoryFindDto;
+import com.bank.card_history.dto.TotalByMonth;
 import com.bank.card_history.entity.CardHistory;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -26,12 +27,26 @@ public class CardHistoryRepositoryImpl implements CardHistoryRepositoryCustom {
     }
 
     @Override
-    public List<CategoryDto> getCardHistoryByCategory(List<Integer> userCardList, LocalDateTime startDate,LocalDateTime endDate) {
+    public List<CategoryDto> getCardHistoryByCategory(List<Integer> userCardList, LocalDateTime startDate, LocalDateTime endDate) {
         return jpaQueryFactory.select(Projections.fields(CategoryDto.class,
-                cardHistory.category, cardHistory.amount.sum().as("price"), cardHistory.discountAmount.sum().as("discountAmount")))
+                        cardHistory.category,
+                        cardHistory.amount.sum().as("price"),
+                        cardHistory.discountAmount.sum().as("discountAmount")))
                 .from(cardHistory)
-                .where(cardHistory.userCard.id.in(userCardList), cardHistory.transactionDate.between(startDate,endDate))
+                .where(cardHistory.userCard.id.in(userCardList), cardHistory.transactionDate.between(startDate, endDate))
                 .groupBy(cardHistory.category)
                 .fetch();
+    }
+
+    public TotalByMonth getTotalByMonth(List<Integer> cardUuidList, int month) {
+        return jpaQueryFactory
+                .select(Projections.fields(TotalByMonth.class,
+                        cardHistory.amount.sum().as("totalPayByMonth"),
+                        cardHistory.discountAmount.sum().as("totalDiscountByMonth")))
+                .from(cardHistory)
+                .where(cardHistory.userCard.id.in(cardUuidList),
+                        cardHistory.transactionDate.month().eq(month))
+                .fetchOne();
+
     }
 }
