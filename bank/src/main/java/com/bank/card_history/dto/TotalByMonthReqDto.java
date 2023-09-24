7fa9885996be1ecd9d2001f3da59839fab66d5a6
한성현 <hsh5349@kakao.com>
2023-09-25 00:33:09 +0900

    feat : 반환 dto 추가

--- bank/src/main/java/com/bank/card_history/dto/TotalByMonthReqDto.java
+++ bank/src/main/java/com/bank/card_history/dto/TotalByMonthReqDto.java
@@ -1,11 +1,15 @@
 package com.bank.card_history.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
+@Setter
+@AllArgsConstructor
 @NoArgsConstructor
 public class TotalByMonthReqDto {
     private List<Integer> cardUuidList;
