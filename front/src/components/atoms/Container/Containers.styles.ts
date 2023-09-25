4309de050d043ef9b720c70a85830a5f72a58e8d
“배민경” <hellobaemin@gmail.com>
2023-09-25 14:58:24 +0900

    docs : 시간,  새로고침 아이콘, QR 토글 화면 뿌리기

--- front/src/components/atoms/Container/Containers.styles.ts
+++ front/src/components/atoms/Container/Containers.styles.ts
@@ -15,7 +15,7 @@ export const BarcodeWhiteContainer = styled(Container)`
   margin-top: 36px;
   background-color: white;
   border-radius: 38px;
-  height: 388px;
+  height: 350px;
   position: relative;
   top: 80px;
 `;
