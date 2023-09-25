4a5911aa44b2fa6ded46cdf2b9bcb011aeeb6fa2
hwanhee <dup07168@naver.com>
2023-09-25 21:09:58 +0900

    refactor : 통계 값에 퍼센트 추가

--- bank/src/main/java/com/bank/card_history/repository/CardHistoryRepositoryImpl.java
+++ bank/src/main/java/com/bank/card_history/repository/CardHistoryRepositoryImpl.java
@@ -8,7 +8,6 @@ import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import static com.bank.card_history.entity.QCardHistory.cardHistory;
@@ -27,13 +26,13 @@ public class CardHistoryRepositoryImpl implements CardHistoryRepositoryCustom {
     }
 
     @Override
-    public List<CategoryDto> getCardHistoryByCategory(List<Integer> userCardList, LocalDateTime startDate, LocalDateTime endDate) {
+    public List<CategoryDto> getCardHistoryByCategory(List<Integer> userCardList, double totalPayByMonth, double totalDiscountByMonth, int month) {
+        System.out.println(totalPayByMonth);
+        System.out.println(totalDiscountByMonth);
         return jpaQueryFactory.select(Projections.fields(CategoryDto.class,
-                        cardHistory.category,
-                        cardHistory.amount.sum().as("price"),
-                        cardHistory.discountAmount.sum().as("discountAmount")))
+                cardHistory.category, cardHistory.amount.sum().as("price"), cardHistory.discountAmount.sum().as("discountAmount")))
                 .from(cardHistory)
-                .where(cardHistory.userCard.id.in(userCardList), cardHistory.transactionDate.between(startDate, endDate))
+                .where(cardHistory.userCard.id.in(userCardList), cardHistory.transactionDate.month().eq(month))
                 .groupBy(cardHistory.category)
                 .fetch();
     }
@@ -41,8 +40,8 @@ public class CardHistoryRepositoryImpl implements CardHistoryRepositoryCustom {
     public TotalByMonth getTotalByMonth(List<Integer> cardUuidList, int month) {
         return jpaQueryFactory
                 .select(Projections.fields(TotalByMonth.class,
-                        cardHistory.amount.sum().as("totalPayByMonth"),
-                        cardHistory.discountAmount.sum().as("totalDiscountByMonth")))
+                        cardHistory.amount.sum().doubleValue().as("totalPayByMonth"),
+                        cardHistory.discountAmount.sum().doubleValue().as("totalDiscountByMonth")))
                 .from(cardHistory)
                 .where(cardHistory.userCard.id.in(cardUuidList),
                         cardHistory.transactionDate.month().eq(month))
