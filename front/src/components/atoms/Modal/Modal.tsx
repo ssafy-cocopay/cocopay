6304f23a1b88a9bce1a67c3e602d7b7b9a5c9dd5
“배민경” <hellobaemin@gmail.com>
2023-09-26 10:17:49 +0900

    feat : 모달 개발

--- front/src/components/atoms/Modal/Modal.tsx
+++ front/src/components/atoms/Modal/Modal.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { ModalContent } from "@/components/atoms/Modal/Modal.styles";
+import { Text } from "@/components/atoms/Text/Text.styles";
 
-const ModalComponent = () => {
+interface ModalProps {
+  toggleModal: () => void;
+}
+
+const Modal = ({ toggleModal }: ModalProps) => {
   return (
     <>
       <ModalContent onClick={(e) => e.stopPropagation()}>
-        임시모달~ 이것은 모달의 내용입니다.
-        {/* <button onClick={toggleModal}>닫기</button> */}
+        <Text>임시모달~ 이것은 모달의 내용입니다.</Text>
+        <button onClick={toggleModal}>닫기</button>
       </ModalContent>
     </>
   );
 };
 
-export default ModalComponent;
+export default Modal;
