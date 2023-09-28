3d99571fe6593106fbf9fa78f148eab9ebafafe4
“배민경” <hellobaemin@gmail.com>
2023-09-28 14:14:12 +0900

    feat : instance.ts 추가, Axios 기본 인스턴스 생성

--- front/src/apis/instance.ts
+++ front/src/apis/instance.ts
@@ -1,11 +1,14 @@
-import { PATH } from '@/constants/path';
-import axios, { Axios, AxiosError, AxiosResponse } from 'axios';
+import axios, { Axios } from "axios";
 
+// to.. 리액트 쿼리 느낌코딩 예정 서영이와 혜혀니 
+// Axios를 사용하여 기본 설정을 가진 인스턴스 생성
 const instance: Axios = axios.create({
-  baseURL: `${process.env.REACT_APP_SERVER_URL}/api`,
-  withCredentials: true,
+  baseURL: `${process.env.REACT_APP_SERVER_URL}/api`, // 여기 오류나면 https://j9b208.p.ssafy.io/api로 변경
+  withCredentials: true, //  요청과 응답에 쿠키 포함
   headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${localStorage.getItem('accessToken') || undefined}`,
+    "Content-Type": "application/json",
+    Authorization: `${localStorage.getItem("userId") || undefined}`, // 여기 오류나면 userId 냅다 1로 해보기
   },
 });
+
+export { instance };
