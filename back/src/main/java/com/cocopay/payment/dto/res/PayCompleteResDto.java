ca5f3f5f8f9481faa704958cbfbd6f8122c4e447
한성현 <hsh5349@kakao.com>
2023-09-29 20:28:46 +0900

    rename : 생색내기 페이지 반환 dto 이름 변경

--- back/src/main/java/com/cocopay/payment/dto/res/PayCompleteResDto.java
+++ back/src/main/java/com/cocopay/payment/dto/res/PayCompleteResDto.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class PayAfterResDto {
+public class PayCompleteResDto {
     private String cardImage;
     private String cardName;
     private int discounted;
