e1a099b71e276ce49f4ebfaea73e7592cd2383fd
홍지민 <baker0929@naver.com>
2023-10-02 22:42:40 +0900

    feat : 임시저장

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,18 @@ import React, { Suspense } from "react";
 import { LandingPage } from "@/pages/LandingPage/LandingPage";
 import { Text } from "@/components/atoms/Text/Text.styles";
 
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCVMfJu-9DESjwRqI9ZnIXCqXi3LArS2v4",
+  authDomain: "ssuk-ssuk-push-server.firebaseapp.com",
+  projectId: "ssuk-ssuk-push-server",
+  storageBucket: "ssuk-ssuk-push-server.appspot.com",
+  messagingSenderId: "783105738433",
+  appId: "1:783105738433:web:bf9038b74de37df79db640",
+  measurementId: "G-G26E58EJJG"
+};
+
 function App() {
   return (
     <Suspense fallback={<Text>로딩중</Text>}>
