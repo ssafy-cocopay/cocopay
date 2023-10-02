9e03ba07b22d05d36312702934c4d04f86bae082
“배민경” <hellobaemin@gmail.com>
2023-10-02 22:00:58 +0900

    feat : QR CAMERA 시도

--- front/src/components/organisms/FourCategory/FourCategory.tsx
+++ front/src/components/organisms/FourCategory/FourCategory.tsx
@@ -5,7 +5,7 @@ import CategoryBox from "@/components/organisms/CategoryBox/CategoryBox";
 export const FourCategory = () => {
   // TODO: API로 상위 카테고리 4개
   const fourCategory = [
-    { num: 89, category: "주유", amount: 4970 },
+    { num: 25.6, category: "카페", amount: 4970 },
     { num: 76, category: "온라인쇼핑", amount: 3220 },
     { num: 49, category: "영화", amount: 970 },
     { num: 24, category: "대중교통", amount: 30 },
