3e2e88c3b64d64654ad01b1ac2f65231db6553f1
“배민경” <hellobaemin@gmail.com>
2023-10-04 23:46:20 +0900

    feat : 이벤트 리스너 App에 추가

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,21 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect } from "react";
 import { LandingPage } from "@/pages/LandingPage/LandingPage";
 import { Text } from "@/components/atoms/Text/Text.styles";
 
 const App = () => {
-  document.addEventListener("message", (e: any) => alert(e.data));
+  useEffect(() => {
+    // 이벤트 리스너 추가
+    const handleMessage = (e: any) => {
+      console.log(e.data); // React Native로부터 받은 메시지 출력
+    };
+
+    window.addEventListener("message", handleMessage);
+
+    // 컴포넌트 unmount 시에 이벤트 리스너 제거
+    return () => {
+      window.removeEventListener("message", handleMessage);
+    };
+  }, []);
 
   return (
     <Suspense fallback={<Text>로딩중</Text>}>
