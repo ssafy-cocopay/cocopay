2b7cdba186cabb8a1cd33e08cdb77c9ba9c5c9e0
“배민경” <hellobaemin@gmail.com>
2023-10-05 16:57:16 +0900

    feat : 이벤트리스너 예외조건 변경

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,26 +1,16 @@
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense } from "react";
 import { LandingPage } from "@/pages/LandingPage/LandingPage";
 import { Text } from "@/components/atoms/Text/Text.styles";
 
 const App = () => {
-  window.addEventListener('message', function(event) {
-    if (event.origin !== 'http://172.22.128.1:3000') {
-        return;
-    }
-
-    // Parse the data received
+  window.addEventListener("message", function (event) {
     const data = event.data;
-
-    // Now you can use the received data (e.g., the FCM token)
-    console.log('Received message:', data);
-
-    // ... Handle the data as needed ...
-});
-
+    console.log("Received message:", data);
+  });
 
   return (
     <Suspense fallback={<Text>로딩중</Text>}>
-      <LandingPage/>
+      <LandingPage />
     </Suspense>
   );
 };
