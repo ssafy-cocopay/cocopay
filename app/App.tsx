bc84ca13a70211fa3f0370731022dcf33db1dc39
홍지민 <baker0929@naver.com>
2023-10-05 16:17:33 +0900

    망자의 흔적들,,

--- app/App.tsx
+++ app/App.tsx
@@ -10,6 +10,7 @@ import {Camera, useCameraDevices} from 'react-native-vision-camera';
 import {PermissionsAndroid, Alert} from 'react-native';
 import {check, PERMISSIONS, RESULTS, request} from 'react-native-permissions';
 import messaging from '@react-native-firebase/messaging'; // 리액트 메시징
+import {firebase} from '@react-native-firebase/dynamic-links';
 
 import {
   WebViewErrorEvent,
@@ -32,39 +33,73 @@ async function requestCameraPermission() {
 }
 
 const App = () => {
-  useEffect(() => {
-    requestUserPermission();
-    //requestCameraPermission();
-    //requestPermission();
-    const unsubscribe = messaging().onMessage(async (remoteMessage : any) => {
-      console.log("푸쉬알림 날라옴 : ", remoteMessage);
-      setRemoteMessage(remoteMessage);//
-      //webviewRef.current.postMessage(remoteMessage.data.description);
-
-      return () => unsubscribe();
-    })
-  }, []);
-
   //웹뷰와 RN과의 소통은 아래의 ref 값을 이용하여 이루어진다
-  let webviewRef = useRef<any>(null);
+  let webviewRef = useRef<any>();
   const devices = useCameraDevices(); // 사용 가능한 카메라 디바이스 목록을 가져옵니다.
   const backCamera = devices.find(device => device.position === 'back'); // 후면 카메라를 선택합니다.
   const [showCamera, setShowCamera] = useState(false);
   const [fcmToken, setFcmToken] = useState<string | null>(null);
-  const [remoteMessage, setRemoteMessage] = useState<any | null>(null);
+  const [remoteMessage, setRemoteMessage] = useState(false);
+
+  function navigateToURL(url : any) {
+    if (webviewRef.current) {
+      const jsCode = `window.location.href = "${url}";`;
+      webviewRef.current.injectJavaScript(jsCode);
+    }
+  }
+
+  useEffect(() => {
+  //   //requestUserPermission();
+  //   //requestCameraPermission();
+  //   //requestPermission();
+  //   const unsubscribe = messaging().onMessage(async (remoteMessage : any) => {
+  //     console.log("푸쉬알림 날라옴 : ", remoteMessage);
+  //     setRemoteMessage(remoteMessage);//
+  //     try {
+  //     webviewRef.current.postMessage(remoteMessage.data.description);
+  //     console.log("보냈어요");;;
+  //     } catch (e)
+  //     {
+  //       console.log("나 에러 ㅎㅇ");
+  //     }
+
+  //     return () => unsubscribe();
+  //   })
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = firebase.dynamicLinks().onLink((link) => {
+      console.log(link.url);
+      // WebView의 URL을 동적 링크의 URL로 설정하여 웹뷰 페이지로 이동합니다.
+      navigateToURL(link.url);
+      console.log("hi");;
+
+      firebase.dynamicLinks()
+      .getInitialLink()
+      .then(link => {
+        if (link && webviewRef.current) {
+          webviewRef.current.loadUrl(link.url);
+        }
+      });
+
+    return () => unsubscribe();
+    });
+  },[])
 
 
 
-    // WebView 로딩 완료시
-    // const handleEndLoading = (e: WebViewNavigationEvent | WebViewErrorEvent) => {
-    //   console.log('handleEndLoading', e);
-    //   // 웹뷰가 로딩 완료되었을 때, 저장된 remoteMessage가 있다면 전송
-    //   if (remoteMessage) {
-    //     webviewRef.current.postMessage(remoteMessage.data.description);
-    //     // 메시지 전송 후 remoteMessage 초기화
-    //     // setRemoteMessage(null);
-    //   }
-    // };
+
+
+    //WebView 로딩 완료시
+    const handleEndLoading = (e: WebViewNavigationEvent | WebViewErrorEvent) => {
+      console.log('handleEndLoading', e);
+      // 웹뷰가 로딩 완료되었을 때, 저장된 remoteMessage가 있다면 전송
+      if (remoteMessage) {
+        //webviewRef.current.postMessage(remoteMessage.data.description);
+        // 메시지 전송 후 remoteMessage 초기화
+        setRemoteMessage(true);
+      }
+    };
 
 
   const qrCamera = () => {
@@ -76,12 +111,12 @@ const App = () => {
   };
 
   // WebView 로딩 완료시
-  const handleEndLoading = (e: WebViewNavigationEvent | WebViewErrorEvent) => {
-    console.log('handleEndLoading', e);
-    // RN에서 웹뷰로 정보 보내는 메소드
-    webviewRef.current.postMessage(fcmToken);
-    console.log("webLoading 완료");
-  };
+  // const handleEndLoading = (e: WebViewNavigationEvent | WebViewErrorEvent) => {
+  //   console.log('handleEndLoading', e);
+  //   // RN에서 웹뷰로 정보 보내는 메소드
+  //   webviewRef.current?.postMessage(fcmToken);
+  //   console.log("webLoading 완료");//
+  // };
 
   enum Event {
     BIO_AUTH,
@@ -105,24 +140,24 @@ const App = () => {
     // data에 웹뷰에서 보낸 값이 들어옵니다.
     console.log(e.nativeEvent.data);
 
-    const event = convertToEvent(e.nativeEvent.data);
-    if (event !== undefined) {
-      switch (event) {
-        case Event.BIO_AUTH:
-          await handleBioAuth();
-          break;
-        case Event.QR_CAMERA: // 이 부분을 추가합니다.
-          qrCamera();
-          break;
-        // 필요하다면 다음 actions 작성은 여기에
-      }
-    }
+    // const event = convertToEvent(e.nativeEvent.data);
+    // if (event !== undefined) {
+    //   switch (event) {
+    //     case Event.BIO_AUTH:
+    //       await handleBioAuth();
+    //       break;
+    //     case Event.QR_CAMERA: // 이 부분을 추가합니다.
+    //       qrCamera();
+    //       break;
+    //     // 필요하다면 다음 actions 작성은 여기에
+    //   }
+    // }
   };
 
-  const handleBioAuth = () => {
-    //complete
-    webviewRef.current?.postMessage('SUCCESS');
-  };
+  // const handleBioAuth = () => {
+  //   //complete
+  //   webviewRef.current?.postMessage('SUCCESS');
+  // };
 
   //FCM
   const getToken = async () => {
@@ -131,7 +166,7 @@ const App = () => {
     try {
       console.log("FCM TOKEN : + " + fcmToken);
       setFcmToken(fcmToken);
-//      webviewRef.current.postMessage(fcmToken);
+      webviewRef.current.postMessage(fcmToken);
     } catch (e) {
       console.log(e, 'Error -------------------------------------------');
     }
@@ -232,10 +267,10 @@ const App = () => {
         onLoadEnd={handleEndLoading}
         onMessage={handleOnMessage}
         webviewRef={webviewRef}
-        sharedCookiesEnabled={true}
-        domStorageEnabled={true}
-        allowFileAccess={true}
-        javaScriptEnabledAndroid={true}
+        //sharedCookiesEnabled={true}//
+        //domStorageEnabled={true}
+        //allowFileAccess={true}
+        //javaScriptEnabledAndroid={true}
         // source={{uri: 'http://localhost:3000'}}
         // source={{uri: 'http://172.28.128.1:3000'}}
         source={{uri: 'https://j9b208.p.ssafy.io/'}}
